f5c65079637c468f62e19b5cc58c09505ffb1715
Albert Richard Dammara <67355005+rdammara@users.noreply.github.com>
2022-10-14 14:32:23 +0700

    Initial File

---
+++ LabPractice/app/src/main/java/com/quiz_00000061865/labpractice/MainActivity.java
@@ -0,0 +1,14 @@
+package com.quiz_00000061865.labpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
